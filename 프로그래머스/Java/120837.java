ba067c9454a947a6f9427837ed056a68fe810613
jakego1 <jakego13129@gmail.com>
2025-02-16 22:54:00 +0900

    120837

---
+++ 프로그래머스/Java/120837.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int hp) {
+        int count = 0;
+        
+        count += hp / 5;
+        hp = hp % 5;
+        
+        count += hp / 3;
+        hp = hp % 3;
+        
+        count += hp;
+        
+        return count;
+    }
+}
