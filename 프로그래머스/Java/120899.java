235524577136c081b0574b29473e2f571f7010c2
jakego1 <jakego13129@gmail.com>
2025-04-26 15:01:54 +0900

    120899

---
+++ 프로그래머스/Java/120899.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int maxValue = array[0];
+        int maxIndex = 0;
+        
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+                maxIndex = i;
+            }
+        }
+        
+        int[] answer = {maxValue, maxIndex};
+        return answer;
+    }
+}
