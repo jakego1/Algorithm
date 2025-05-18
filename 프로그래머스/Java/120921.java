e8f0d69e337c20c67f503238c68f39ca443cd084
jakego1 <jakego13129@gmail.com>
2025-05-19 08:18:10 +0900

    120921

---
+++ 프로그래머스/Java/120921.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(String A, String B) {
+
+        if (A.length() != B.length()) {
+            return -1;
+        }
+
+        if (A.equals(B)) {
+            return 0;
+        }
+        
+        int length = A.length();
+        
+        for (int i = 1; i <= length; i++) {
+
+            A = A.charAt(length - 1) + A.substring(0, length - 1);
+
+            if (A.equals(B)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
