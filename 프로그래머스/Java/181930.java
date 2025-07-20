6451fa34d90992ca3558900446f010177b41f71c
jakego1 <jakego13129@gmail.com>
2025-07-20 14:14:26 +0900

    181930

---
+++ 프로그래머스/Java/181930.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int sum = a + b + c;
+        int squareSum = a*a + b*b + c*c;
+        int cubeSum = a*a*a + b*b*b + c*c*c;
+        
+        if (a == b && b == c) {
+            return sum * squareSum * cubeSum;
+        } else if (a == b || b == c || a == c) {
+            return sum * squareSum;
+        } else {
+            return sum;
+        }
+    }
+}
