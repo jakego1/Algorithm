915b4d05238067d315a03b1be0cd555e671cc2be
jakego1 <jakego13129@gmail.com>
2025-08-12 10:32:12 +0900

    181909

---
+++ 프로그래머스/Java/181912.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (String str : intStrs) {
+            String subStr = str.substring(s, s + l);
+            int num = Integer.parseInt(subStr);
+            if (num > k) {
+                result.add(num);
+            }
+        }
+        
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
