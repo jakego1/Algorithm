4ac51f51cf936651c7d0edbd91f6cb4e39d1047a
jakego1 <jakego13129@gmail.com>
2025-09-11 21:55:38 +0900

    181859

---
+++ 프로그래머스/Java/181859.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stk = new Stack<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (stk.isEmpty()) {
+                stk.push(arr[i]);
+            } else if (stk.peek() == arr[i]) {
+                stk.pop();
+            } else {
+                stk.push(arr[i]);
+            }
+        }
+        
+        if (stk.isEmpty()) {
+            return new int[]{-1};
+        } else {
+            int[] answer = new int[stk.size()];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = stk.get(i);
+            }
+            return answer;
+        }
+    }
+}
