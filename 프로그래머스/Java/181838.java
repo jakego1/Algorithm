0289f9ccb014fd8c1416c0d00dbebaca86bc5a07
jakego1 <jakego13129@gmail.com>
2025-10-09 21:30:50 +0900

    181838

---
+++ 프로그래머스/Java/181838.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        int answer = 0;
+        
+        for(int i = 0; i < 3; i++) {
+            if (date1[i] != date2[i]) {
+                return date1[i] < date2[i] ? 1 : 0;
+            }
+        }
+
+        return answer;
+    }
+}
